518f3acc7fb006b51308a2c0c7176654e3cf25d4
Theodore DeRego <tderego94@gmail.com>
2015-07-23 17:04:41 -1000

    Read stuff from 48 bus, need to correct resistance values

--- main_control/volt_amp.h
+++ main_control/volt_amp.h
@@ -12,11 +12,12 @@ float read_voltage(int pin, const float r1, const float r2)
 
 void send_va_data()
 {
+    float v_h_48 = read_voltage(A9, 19850.0, 1946.0);
     float v_h_24 = read_voltage(A3, 19850.0, 1946.0);
     float v_p_12_e = read_voltage(A2, 19890.0, 1951.0);
     float v_p_12_pl = read_voltage(A7, 19890.0, 1994.0);
 
-    String str = String(v_h_24)+":"+String(v_p_12_e)+":"+String(v_p_12_pl);
+    String str = String(v_h_48)+":"+String(v_h_24)+":"+String(v_p_12_e)+":"+String(v_p_12_pl);
 
     Bridge.put("VOLT", str.c_str());
 }
