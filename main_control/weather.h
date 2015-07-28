4def5cdda797a8bfa5513ca853e54b443de8df61
Casey Pearring <pearring@hawaii.edu>
2015-07-27 14:18:16 -1000

    anemometer

--- main_control/weather.h
+++ main_control/weather.h
@@ -17,13 +17,13 @@ void init_weather()
 float read_anemometer(int sensor_pin)
 {
     // Read anemometer voltage
-    int reading = analogRead(sensor_pin);
+    float reading = analogRead(sensor_pin);
 
     // Convert reading to voltage
-    float voltage = float(reading) * (2.0 / 1023.0);
-
+    //float voltage = float(reading) * (2.0 / 1023.0);
+    float wind_speed = ((reading - 204.8) * 2.0) / 1024/0);
     // Calculate wind speed from voltage
-    float wind_speed = voltage - .4;
+    //float wind_speed = voltage - .4;
     if (wind_speed <= 0)
     {
       return wind_speed = 0;
